63e3d3e45811d858276356cb6223eec5d2c6244f
neileverette <neil.everette@gmail.com>
2014-02-23 14:11:17 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+$('document').ready(function(e) {
+    
+	//CREATES RIDGES
+	function createRidges(){
+		
+		// VARIABLES FOR THE FUNCTION
+		var channelsHeight = parseFloat($('.channels').css("height"));
+		var ridgeMargin = parseFloat($('.ridge').css("margin-top"));
+		var ridgeHeight = $('.ridge').height();
+		var ridge = parseInt(ridgeMargin) + parseInt(ridgeHeight);
+		var totalRidges = channelsHeight/ridge;
+		var container = $('.channels');
+		
+		// LOOP THAT CREATES THE RIDGES
+		for (var i = 0; i < totalRidges; i++){
+			container.append("<div class = \"ridge\" id = \"" + i + "\"></div>");
+
+		}	
+	}
+	
+	
+	createRidges();
+});
