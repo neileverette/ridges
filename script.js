0df6d9b94e7e887c8ca4b047d1ef4a721b0a5f1d
neileverette <neil.everette@gmail.com>
2014-02-23 16:45:58 -0800

    Used fixed units for PhoneGap

--- script.js
+++ script.js
@@ -19,7 +19,21 @@ $('document').ready(function(e) {
 
 		}	
 	}
+
+	// CREATES RIDGES BASED ON FIXED DIMENSIONS
+		var channelsHeight = 1440;
+		var ridgeMargin = parseFloat($('.ridge').css("margin-top"));
+		var ridgeHeight = $('.ridge').height();
+		var ridge = parseInt(ridgeMargin) + parseInt(ridgeHeight);
+		var totalRidges = channelsHeight/ridge;
+		var container = $('.channels');
 	
+	// LOOP THAT CREATES THE RIDGES
+		for (var i = 0; i < totalRidges; i++){
+			container.append("<div class = \"ridge\" id = \"" + i + "\"></div>");
+
+		}	
 	
-	createRidges();
+	//createRidges();
+	createRidgesFixed();
 });
