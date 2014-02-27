059d10ef33786d9a3444750dd4bf10c107c7283b
neileverette <neil.everette@gmail.com>
2014-02-27 06:24:20 -0800

    fixed grid logic

--- script.js
+++ script.js
@@ -6,34 +6,38 @@ $('document').ready(function(e) {
 	function createRidges(){
 		
 		// VARIABLES FOR THE FUNCTION
-		var channelsHeight = parseFloat($('.channels').css("height"));
-		var ridgeMargin = parseFloat($('.ridge').css("margin-top"));
-		var ridgeHeight = $('.ridge').height();
-		var ridge = parseInt(ridgeMargin) + parseInt(ridgeHeight);
-		var totalRidges = channelsHeight/ridge;
-		var container = $('.channels');
+		var previewSize = .4;
+		var channelSize = 1-previewSize;
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
 		
-		// LOOP THAT CREATES THE RIDGES
-		for (var i = 0; i < totalRidges; i++){
-			container.append("<div class = \"ridge\" id = \"" + i + "\"></div>");
-
+		// SET CSS STYLES
+		$('.preview').css("height", windowHeight*previewSize);  //previewSize*windowHeight
+		$('.channels').css("height", windowHeight*channelSize);  //previewSize*windowHeight
+		
+		// VARIABLES FOR THE FUNCTION
+		//var channelsHeight = parseFloat($('.channels').css("height"));
+		var ridgeMargin = parseFloat($('.ridge').css("margin-top"));
+		var ridgeHeight = parseFloat($('.ridge').css("height"))
+		var totalRidge = ridgeMargin + ridgeHeight;
+		var numberOfRidges = channelSize*windowHeight/totalRidge;
+		
+		console.log("Window height is " + windowHeight);
+		console.log("Window width is " + windowWidth);
+		console.log("Preview panel is  " + windowHeight*previewSize);
+		console.log("Channels panel is " + windowHeight*channelSize);
+		console.log("Ridget height is "+  ridgeHeight);
+		console.log("Ridget margin is "+  ridgeMargin);
+		console.log("Total ridge size is "+  totalRidge);	
+		console.log("Number of ridges that fits is " + numberOfRidges );
+	
+		
+		//LOOP THAT CREATES THE RIDGES
+		for (var i = 0; i < numberOfRidges; i++){
+			$('.channels').append("<div class = \"ridge\" id = \"" + i + "\"></div>");
 		}	
 	}
 
-	// CREATES RIDGES BASED ON FIXED DIMENSIONS
-		var channelsHeight = 1440;
-		var ridgeMargin = parseFloat($('.ridge').css("margin-top"));
-		var ridgeHeight = $('.ridge').height();
-		var ridge = parseInt(ridgeMargin) + parseInt(ridgeHeight);
-		var totalRidges = channelsHeight/ridge;
-		var container = $('.channels');
 	
-	// LOOP THAT CREATES THE RIDGES
-		for (var i = 0; i < totalRidges; i++){
-			container.append("<div class = \"ridge\" id = \"" + i + "\"></div>");
-
-		}	
-	
-	//createRidges();
-	createRidgesFixed();
+	createRidges();
 });
